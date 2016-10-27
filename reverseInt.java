ea7b818bf2ad9549ea4c6f4dde9db8a70c66d796
Annie Zhou <annie_0812@live.ca>
2016-10-26 23:43:54 -0400

    reverseInt Leetcode Problem

---
+++ reverseInt.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int reverse(int x) {
+        int result = reverseone (Math.abs(x));
+        
+        if (x < 0){
+             return result*-1;
+        }
+        else{
+            return result;
+        }
+    }
+    
+    private int reverseone (int x){
+        int result = 0;
+        
+        if (x == 0){
+            return result;
+        }
+        else {
+            while (x > 0){
+                 if (result < 0 || result > Integer.MAX_VALUE/10 || (result==(Integer.MAX_VALUE/10) && (x%10>Integer.MAX_VALUE%10))){
+                    return 0;
+                 }
+                result = result * 10 + x%10;
+                x /= 10;
+            }
+        }
+        return result;
+    }
+
+}
