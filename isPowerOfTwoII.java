f08a08ea83ffe9f155d8232946755e80a72ebb1d
Annie Zhou <annie_0812@live.ca>
2016-11-24 11:16:32 -0500

    second version of isPowerOfTwo - using while loop to divide 2's everytime - runtime error

---
+++ isPowerOfTwoII.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+       while (n%2 == 0){
+           n = n/2;
+       }
+       if (n == 1) return true;
+       return false;
+    }
+}
