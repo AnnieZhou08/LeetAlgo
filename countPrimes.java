469e15fb00285784aa4059973080e53b7416ad49
Annie Zhou <annie_0812@live.ca>
2016-12-01 23:22:01 -0500

    countPrimes.java LeetCode Solution O(nlogn)

---
+++ countPrimes.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int countPrimes(int n) {
+        boolean [] isPrime = new boolean[n];
+        int count = 0;
+        
+        for (int i = 2; i < n; i++){
+            isPrime[i] = true;
+        }
+        
+        for (int i = 2; i * i < n; i++){
+            if (!isPrime[i]){
+                continue;
+            } else {
+                for (int j = i * i; j < n; j += i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        for (int i = 2; i < n; i++){
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
