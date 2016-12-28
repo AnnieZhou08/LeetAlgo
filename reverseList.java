6783fc94c6f8bb27a9beaa4d5e02996491224454
Annie Zhou <annie_0812@live.ca>
2016-12-28 11:02:37 -0800

    reverses singly linked list by iteration

---
+++ reverseList.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        if (head == null) return null;
+        while (head != null){
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
