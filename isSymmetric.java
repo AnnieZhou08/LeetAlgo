0ae8bf2e3a3d2712f431c488802afe0239805322
Annie Zhou <annie_0812@live.ca>
2016-12-30 19:37:06 -0800

    checks if a binary tree is symmetric

---
+++ isSymmetric.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        
+        return isSymHelper(root.left, root.right);
+    }
+    public boolean isSymHelper(TreeNode root1, TreeNode root2){
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        if (root1.val != root2.val) return false;
+        
+        return isSymHelper(root1.left, root2.right) && isSymHelper(root1.right, root2.left);
+    }
+}
