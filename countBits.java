e6d67620ed680d22ca96de70f0918a51a539696b
Annie Zhou <annie_0812@live.ca>
2017-01-10 12:41:30 -0500

    returns an array of number of bits of each number from 0 to n

---
+++ countBits.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int[] countBits(int num) {
+        int[] res = new int[num+1];
+        for (int i = 0; i <= num; i++){
+            res[i] = Integer.bitCount(i);
+        }
+        return res;
+    }
+}
