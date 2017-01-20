c540cf69e890c893906ed7eaa938408618abb8c1
Annie Zhou <annie_0812@live.ca>
2017-01-20 17:43:12 -0500

    uniquePath - combinatorics

---
+++ uniquePath.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        int N = n+m-2;
+        int k = m-1;
+        long res = 1;
+        
+        for (int i = 1; i <= k; i++){
+            res = res*(N-k+i)/i;
+        }
+        return (int)res;
+    }
+}
