c9e61dcc4884773061b36c0165ed1c0233ab39c0
Annie Zhou <annie_0812@live.ca>
2017-03-05 18:36:52 -0500

    if it's the only black pixel in its col and row then it's a lonely pixel

---
+++ findLonelyPixel.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int findLonelyPixel(char[][] picture) {
+        int [] row = new int[picture.length];
+        int [] col = new int[picture[0].length];
+        
+        for(int i = 0; i < picture.length; i++){
+            for(int j = 0; j < picture[0].length; j++){
+                if(picture[i][j] == 'B'){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        
+        int count = 0;
+        
+        for(int i = 0; i < picture.length; i++){
+            for(int j = 0; j < picture[0].length; j++){
+                if(picture[i][j] == 'B'){
+                    if(row[i] == 1 && col[j] == 1){
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+}
